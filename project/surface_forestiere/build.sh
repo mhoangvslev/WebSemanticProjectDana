f4d004fd5c3cb624180e080fbf1c7a9439f00ba1
Minh Hoang DANG <minhhoangdang@hotmail.com>
2019-11-10 16:03:48 +0100

    Add turtle output

--- project/surface_forestiere/build.sh
+++ project/surface_forestiere/build.sh
@@ -1,1 +1,2 @@
 sh ../../bin/tarql --delimiter ";" --ntriples mapping.sparql surface_forestiere.csv > surface_forestiere.rdf
+sh ../../bin/tarql --delimiter ";" mapping.sparql surface_forestiere.csv > surface_forestiere.ttl
