4ea1323eccc04843e8d9a0f30a021f6da1c89b8c
Minh Hoang DANG <minhhoangdang@hotmail.com>
2019-12-10 01:45:53 +0100

    Finalisation

--- project/surface_forestiere/build.sh
+++ project/surface_forestiere/build.sh
@@ -1,2 +1,12 @@
-sh ../../bin/tarql --delimiter ";" --ntriples mapping.sparql surface_forestiere.csv > surface_forestiere.rdf
-sh ../../bin/tarql --delimiter ";" mapping.sparql surface_forestiere.csv > surface_forestiere.ttl
+# Dataset
+sh ../../bin/tarql --delimiter "," --ntriples mapping.sparql surface_forestiere.csv > surface_forestiere.rdf
+sh ../../bin/tarql --delimiter "," mapping.sparql surface_forestiere.csv > surface_forestiere.ttl
+
+
+# Vocabulaire
+sh ../../bin/tarql --delimiter "," --ntriples vocabulary.sparql https://mmisw.org/ont/~mhoangvslev/SurfaceForestiereVoc/ > surface_forestiere_voc.rdf
+sh ../../bin/tarql --delimiter "," vocabulary.sparql https://mmisw.org/ont/~mhoangvslev/SurfaceForestiereVoc/ > surface_forestiere_voc.ttl
+
+# VOID vocabulaire
+sh ../../bin/tarql --delimiter "," --ntriples void_description.sparql https://mmisw.org/ont/~mhoangvslev/SurfaceForestiereVoc/ > void_description.rdf
+sh ../../bin/tarql --delimiter "," void_description.sparql https://mmisw.org/ont/~mhoangvslev/SurfaceForestiereVoc/ > void_description.ttl
